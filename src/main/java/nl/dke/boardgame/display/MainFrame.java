3f37036a5d44fe22ab16ff880f58b1877f7ae09c
BRK-PoGo <tjwally29@gmail.com>
2016-10-03 12:26:30 +0200

    Added new method of user input

--- src/main/java/nl/dke/boardgame/display/MainFrame.java
+++ src/main/java/nl/dke/boardgame/display/MainFrame.java
@@ -12,15 +12,18 @@ public class MainFrame extends JFrame
 
     private static final long serialVersionUID = 1L;
     private MainPanel mainPanel;
+    private InfoPanel infoPanel;
     Board board;
 
     public MainFrame(Board board)
     {
     	this.board = board;
         mainPanel = new MainPanel(board);
+        infoPanel = new InfoPanel();
         setLayout(new BorderLayout());
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         getContentPane().add(mainPanel, BorderLayout.CENTER);
+        getContentPane().add(infoPanel, BorderLayout.EAST);
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
